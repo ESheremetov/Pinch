77aea91da1395f0109745204d78ad27d907e1cde
egor_sheremetov <georgesheremetov3.0@gmail.com>
2022-01-26 10:03:58 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Егор Шереметов on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
