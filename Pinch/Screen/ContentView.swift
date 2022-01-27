46b0a9388016f2a35b6e0cea8b1e73882c82467c
egor_sheremetov <georgesheremetov3.0@gmail.com>
2022-01-27 08:33:37 +0300

    add magnification gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -59,6 +59,27 @@ struct ContentView: View {
                                 }
                             }
                     ) //: GESTURE
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    } else if imageScale < 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale < 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
